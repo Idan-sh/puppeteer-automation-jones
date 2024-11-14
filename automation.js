0545d3bc4384f1848f048a3ae1f87bc6dbcb9169
Idan <104992892+Idan-sh@users.noreply.github.com>
2024-11-14 18:19:51 +0200

    Automation now opens the site and screenshots it. Screenshot is ignored on git.

--- automation.js
+++ automation.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer');
 (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('https://google.com');
+    await page.goto('https://testsite.getjones.com/ExampleForm/');
     await page.screenshot({path: 'screenshot.png'});
     await browser.close();
 })();
